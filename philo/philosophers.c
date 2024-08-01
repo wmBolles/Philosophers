a2457564139f057d84875dc6a90e160cbf46b6e8
wmbolles <wassimbolles18@gmail.com>
2024-08-01 11:20:08 +0100

    init_commit: set-up the codebase

---
+++ philo/philosophers.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wabolles <wabolles@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/01 09:01:51 by wabolles          #+#    #+#             */
+/*   Updated: 2024/08/01 11:17:37 by wabolles         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+#define unused __attribute__ ((unused))
+
+int main(int unused argc, char const unused *argv[])
+{
+	return 0;
+}
